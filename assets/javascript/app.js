d0d669d68d0b58c43e4ba8ce21b649129b12127a
geofell <geofell22@gmail.com>
2019-07-05 19:27:29 -0400

    add js logic to count down from 30 and stop at 0

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+
+// prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+var time = 5;
+
+window.onload = function() {
+  start();
+};
+
+function start() {
+  //  TODO: Use setInterval to start the count here and set the clock to running.
+  if (!clockRunning) {
+    clockRunning = true;
+    intervalId = setInterval(count, 1000);
+  }
+}
+
+function stop() {
+  //  TODO: Use clearInterval to stop the count here and set the clock to not be running.
+  clearInterval(intervalId);
+  clockRunning = false;
+}
+
+function count() {
+  //  TODO: decrease time by 1, remember we cant use "this" here.
+  time--;
+  //  TODO: Get the current time, pass that into the timeConverter function,
+  //        and save the result in a variable.
+  timeConverter(time);
+  //  TODO: Use the variable you just created to show the converted time in the "display" div.
+  var result = timeConverter(time);
+  $("#timer").html(result);
+  if (time === 0) {
+    stop();
+    alert("game over!");
+  }
+}
+
+//  THIS FUNCTION IS DONE FOR US!
+//  We do not need to touch it.
+
+function timeConverter(t) {
+  //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+  var minutes = Math.floor(t / 60);
+  var seconds = t - minutes * 60;
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  } else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
