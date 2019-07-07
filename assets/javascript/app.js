f74afde145b6b13dabc36efc15d684a588023036
geofell <geofell22@gmail.com>
2019-07-06 22:05:41 -0400

    changed the dynamic choice generator to update the divs instead of using .append.  .append was causing the onclick event to not trigger.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,13 @@ function stop() {
   clockRunning = false;
 }
 
+function reset() {
+  time = 30;
+  //  TODO: Change the "display" div to "00:00."
+  stop();
+  $("#timer").html("00:30");
+}
+
 function count() {
   //  decrease time by 1 second
   time--;
@@ -44,13 +51,13 @@ function timeConverter(t) {
 }
 // =============== Timer Functions - END =============== //
 
-function loadQuestion() {
-  $("#question").html(questionAnswer.question);
+function loadQuestion(qa) {
+  $("#question").html(questionAnswer[qa].question);
 }
-function checkWinner(str) {
+function checkWinner(str, qa) {
   bWinner = false;
   console.log("Passed selection:" + str);
-  if (str === questionAnswer.choices[0]) {
+  if (str === questionAnswer[qa].choices[0]) {
     bWinner = true;
   } else {
     bWinner = false;
@@ -58,18 +65,13 @@ function checkWinner(str) {
   return bWinner;
 }
 
-function loadChoices() {
-  for (x = 0; x < questionAnswer.choices.length; x++) {
-    var choice = questionAnswer.choices[x];
-    $("#choices").append(
-      "<div id=choice" +
-        x +
-        " class=choice value='" +
-        choice +
-        "'>" +
-        choice +
-        "</div>"
-    );
+function loadChoices(qa) {
+  //   $("#choices").empty();
+  for (x = 0; x < questionAnswer[qa].choices.length; x++) {
+    var choice = questionAnswer[qa].choices[x];
+    // $("#choices").append("<div id=choice" + x + " class=choice value='" + choice + "'>" + choice + "</div>"
+    $("#choice" + x).attr("value", choice);
+    $("#choice" + x).html(choice);
   }
 }
 
@@ -78,48 +80,108 @@ function loadChoices() {
 //  Variable that will hold our setInterval that runs the stopwatch
 var intervalId;
 var clockRunning = false;
-var time = 5;
+var time = 30;
 var isWinner = false;
-var questionAnswer = {
-  question: "Who was Snow Whites prince?",
-  choices: [
-    "Prince Ferdinand",
-    "Prince Philip",
-    "Prince Charming",
-    "Prince Naveen"
-  ],
-  image: "princeferdinand.jpg",
-  audio: ""
-};
+var questionAnswer = [
+  {
+    category: "princes",
+    question: "Who was Snow Whites prince?",
+    choices: [
+      "Prince Ferdinand",
+      "Prince Philip",
+      "Prince Charming",
+      "Prince Naveen"
+    ],
+    image: "princeferdinand.jpg",
+    audio: ""
+  },
+  {
+    category: "princes",
+    question:
+      "Who was transformed from a frog to a prince after being kissed by Princess Tiana?",
+    choices: [
+      "Prince Naveen",
+      "Prince Philip",
+      "Prince Charming",
+      "Prince Ferdinand"
+    ],
+    image: "princeferdinand.jpg",
+    audio: ""
+  },
+  {
+    category: "princes",
+    question: "How does Aladdin travel to see Princess Jasmine?",
+    choices: ["Magic Carpet", "Horse", "Boat", "Plane"],
+    image: "princeferdinand.jpg",
+    audio: ""
+  },
+  {
+    category: "princes",
+    question: "Where does the beast prohibit Belle from going in his castle?",
+    choices: ["The West Wing", "Kitchen", "Dinning Room", "Main Ballroom"],
+    image: "princeferdinand.jpg",
+    audio: ""
+  },
+  {
+    category: "princes",
+    question: "Where did Arielle rescue prince Eric from?",
+    choices: ["Ocean", "Castle", "Forest", "Cave"],
+    image: "princeferdinand.jpg",
+    audio: ""
+  }
+];
+
+var questionSelected = 0;
+var answered = false;
 
 window.onload = function() {
-  loadQuestion();
-  loadChoices();
+  loadQuestion(questionSelected);
+  loadChoices(questionSelected);
   start();
 
   $(".choice").on("click", function() {
-    stop();
-    userSelection = $(this).attr("value");
-    console.log("User selected: " + userSelection);
-    isWinner = checkWinner(userSelection);
-    console.log("Winner?" + isWinner);
-
-    if (isWinner) {
-      alert("You won!");
-      $(this).css("background-color", "lightgreen");
-      $(this).css("color", "white");
-      $(".answer-container").html(
-        "<img height=80% width=60% src='assets/images/princeferdinand.png' />"
-      );
-      $(".answer-container").css(
-        "background",
-        "url(assets/images/princeferdinand.png)"
-      );
-      $(".answer-container").css("opacity", "1.0");
+    console.log("on click");
+    if (answered) {
+      // After user makes selection disable ability to keep selecting choices
+      console.log("question answered - load next question");
     } else {
-      alert("Wrong choice!");
-      $(this).css("background-color", "red");
-      $(this).css("color", "white");
+      answered = true;
+      stop();
+      userSelection = $(this).attr("value");
+      console.log("User selected: " + userSelection);
+      isWinner = checkWinner(userSelection, questionSelected);
+      console.log("Winner?" + isWinner);
+
+      if (isWinner) {
+        $(this).css("background-color", "lightgreen");
+        alert("You won!");
+        // $(this).css("color", "white");
+        $(".answer-container").html(
+          "<img height=80% width=60% src='assets/images/princeferdinand.png' />"
+        );
+        $(".answer-container").css(
+          "background",
+          "url(assets/images/princeferdinand.png)"
+        );
+        $(".answer-container").css("opacity", "1.0");
+      } else {
+        $(this).css("background-color", "red");
+        alert("Wrong choice!");
+        // $(this).css("color", "white");
+      }
+      if (questionSelected < questionAnswer.length) {
+        setInterval(5000);
+        questionSelected++;
+        answered = false;
+        isWinner = false;
+        clockRunning = false;
+        $(".choice").css("background-color", "transparent");
+        console.log("question selected = " + questionSelected);
+        loadQuestion(questionSelected);
+        loadChoices(questionSelected);
+        reset();
+        start();
+      }
     }
   });
 };
